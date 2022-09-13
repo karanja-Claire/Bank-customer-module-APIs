96e9e1eec6c18e57453917b9e5412ecbb8636c85
ckaranja <clairekaranja99@gmail.com>
2022-09-13 15:54:26 +0300

    -

--- myapi/services/stocks.py
+++ myapi/services/stocks.py
@@ -1,20 +1,3 @@
-import requests
-class StockService():
-
-
-    def currency(self):
-        url = "https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}"
-
-        payload = {}
-        headers= {
-        "apikey": "m4fcBSntFnKkgNDWU51J7JP18GQHiiii"
-        }
-
-        response = requests.request("GET", url, headers=headers, data = payload)
-
-        status_code = response.status_code
-        result = response.text
-
 
 
 
