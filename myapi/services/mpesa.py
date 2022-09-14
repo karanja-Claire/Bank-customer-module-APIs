df4eff307610e0e8cfa125a87abc11fc5542cf3d
CLAIRE KARANJA <87809371+karanja-Claire@users.noreply.github.com>
2022-09-14 21:52:22 +0300

    Eapi (#7)
    * -
    
    * -

---
+++ myapi/services/mpesa.py
@@ -0,0 +1,10 @@
+import requests
+class MpesaService:
+    def auth(self):
+        url = "https://sandbox.safaricom.co.ke/oauth/v1/generate"
+        querystring = {"grant_type":"client_credentials"}
+        payload = ""
+        headers = {"Authorization": "Basic Key"}
+        
+        response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+     
